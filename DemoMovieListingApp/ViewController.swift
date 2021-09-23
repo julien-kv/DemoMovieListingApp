f02a19a67dd5e8471ea94709176235c6034ffaa5
jijusjacob206 <jiju@keyvalue.systems>
2021-09-23 10:47:32 +0530

    Initial Commit

---
+++ DemoMovieListingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoMovieListingApp
+//
+//  Created by Julien on 23/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
